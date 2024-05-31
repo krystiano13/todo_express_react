599aaf131da45841f45c5228e72adb4a48e10eb1
krystiano13 <crystianotv@gmail.com>
2024-05-31 11:10:09 +0200

    implement updating tasks

--- server/src/routes/task.mjs
+++ server/src/routes/task.mjs
@@ -60,4 +60,44 @@ router.post(
   }
 );
 
-router.patch("/api/tasks", userLoggedIn);
+router.patch(
+  "/api/tasks/:id",
+  userLoggedIn,
+  validateTitle,
+  validateIsDone,
+  async (request, response) => {
+    if (!request.params.id) {
+      return response.status(400).send({ error: "ID not found" });
+    }
+
+    const result = validationResult(request);
+
+    if (!result.isEmpty()) {
+      return response.status(400).json({ errors: result.array() });
+    }
+
+    const task = await Task.findOne({ _id: request.params.id });
+
+    if (!task) {
+      return response.status(404).send({ error: "Task not found" });
+    }
+
+    if (request.session.passport.user !== task.email) {
+      return response.status(403).send({
+        message: "Unauthorized access",
+      });
+    }
+
+    try {
+      const updatedTask = await task.updateOne({
+        title: request.body.title,
+        isDone: request.body.isDone,
+      });
+      return response
+        .status(201)
+        .send({ message: "Task updated successfully", task: updatedTask });
+    } catch (error) {
+      return response.status(500).send({ message: "Internal server error" });
+    }
+  }
+);
