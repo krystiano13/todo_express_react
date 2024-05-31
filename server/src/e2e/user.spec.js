1311169f725c7414b27d6b396304d21ace8d63ee
krystiano13 <crystianotv@gmail.com>
2024-05-31 16:30:33 +0200

    create unit test for logging in

--- server/src/e2e/user.spec.js
+++ server/src/e2e/user.spec.js
@@ -24,8 +24,24 @@ describe("users", () => {
     expect(response.statusCode).toBe(200);
   });
 
+  it("should log in, return 200 and show logged in status", async () => {
+    const response_1 = await request(app)
+      .post("/api/auth/login")
+      .send({
+        email: "XNnJc@example.com",
+        password: "test1234",
+      })
+      .then((res) => {
+        return request(app)
+          .get("/api/auth/status")
+          .set("Cookie", res.headers["set-cookie"]);
+      });
+
+    expect(response_1.statusCode).toBe(200);
+  });
+
   afterAll(async () => {
-    await mongoose.connection.dropDatabase;
+    await mongoose.connection.dropDatabase();
     await mongoose.connection.close();
   });
 });
