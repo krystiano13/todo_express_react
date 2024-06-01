8773aa4c5a427a099ee22457390e442af8e6d54c
krystiano13 <crystianotv@gmail.com>
2024-06-01 11:16:45 +0200

    create register view

--- client/src/views/Login.tsx
+++ client/src/views/Login.tsx
@@ -3,16 +3,18 @@ export function Login() {
     <div className="w-full h-full flex justify-center items-center">
       <form className="form-appear flex flex-col items-center gap-10 pt-16 pb-16 pl-8 pr-8 bg-white form-shadow">
         <input
-          className="outline-0 input-shadow p-2 min-w-72 transition-all cursor-pointer"
+          className="outline-0 input-shadow p-2 min-w-72 transition-all"
           type="email"
           placeholder="Your email"
           required
+          name="email"
         />
         <input
-          className="outline-0 input-shadow p-2 min-w-72 transition-all cursor-pointer"
+          className="outline-0 input-shadow p-2 min-w-72 transition-all"
           type="password"
           placeholder="Password"
           required
+          name="password"
         />
         <button
           className="p-2 pl-6 pr-6 text-white bg-emerald-500 min-w-72 hover:bg-emerald-400 transition-colors"
