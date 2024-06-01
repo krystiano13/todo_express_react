3c18adcf35ee02f7d8512e88827dbb19a1469132
krystiano13 <crystianotv@gmail.com>
2024-06-01 13:49:41 +0200

    implement login function on client side

--- client/src/views/Login.tsx
+++ client/src/views/Login.tsx
@@ -1,7 +1,31 @@
 export function Login() {
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const data = await new FormData(e.target as HTMLFormElement);
+
+    await fetch("http://localhost:3000/api/auth/login", {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: data.get("email"),
+        password: data.get("password"),
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+
+    console.log(document.cookie);
+  }
+
   return (
     <div className="w-full h-full flex justify-center items-center">
-      <form className="form-appear flex flex-col items-center gap-10 pt-16 pb-16 pl-8 pr-8 bg-white form-shadow">
+      <form
+        onSubmit={handleSubmit}
+        className="form-appear flex flex-col items-center gap-10 pt-16 pb-16 pl-8 pr-8 bg-white form-shadow"
+      >
         <input
           className="outline-0 input-shadow p-2 min-w-72 transition-all"
           type="email"
--- server/src/app.mjs
+++ server/src/app.mjs
@@ -1,4 +1,6 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import mongoose from "mongoose";
 import passport from "passport";
 import session from "express-session";
@@ -10,10 +12,21 @@ import "./auth/local-strategy.js";
 export function createApp() {
   const app = express();
 
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+      preflightContinue: true,
+      credentials: true,
+    })
+  );
+
+  app.use(cookieParser());
+
   app.use(express.json());
   app.use(
     session({
       secret: "keyboard cat",
+      httpOnly: false,
       resave: false,
       saveUninitialized: true,
       cookie: {
