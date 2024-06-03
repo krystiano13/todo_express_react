74d2ae00d7a2048ea579c5d335fce14f71b49385
krystiano13 <crystianotv@gmail.com>
2024-06-03 10:36:25 +0200

    create function for adding new tasks

--- client/src/utils/tasks.ts
+++ client/src/utils/tasks.ts
@@ -4,7 +4,6 @@ export function getTasks(
   userContext: UserContext,
   setTasks: (tasks: { title: string; isDone: boolean }[]) => void
 ) {
-    
   if (!userContext.User.user) return;
 
   fetch(
@@ -29,6 +28,46 @@ export function getTasks(
         });
 
         setTasks(tasksArray);
+      }
+    });
+}
+
+export function addTask(
+  e: React.FormEvent<HTMLFormElement>,
+  userContext: UserContext,
+  tasks: { title: string; isDone: boolean }[],
+  setTasks: (tasks: { title: string; isDone: boolean }[]) => void
+) {
+  e.preventDefault();
+
+  if (!userContext.User.user) return;
+  const formData = new FormData(e.target as HTMLFormElement);
+
+  fetch(`http://localhost:3000/api/tasks`, {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      title: formData.get("title"),
+      isDone: false,
+    }),
+  })
+    .then((res) => {
+      if (res.status === 500) {
+        alert("Internal server error");
+      }
+
+      if (res.status === 200) {
+        return res.json();
+      }
+    })
+    .then((data) => {
+      if (data.task) {
+        setTasks([...tasks, data.task]);
+        //@ts-ignore
+        e.target.children[0].value = "";
       }
     });
 }
--- client/src/views/Home.tsx
+++ client/src/views/Home.tsx
@@ -1,6 +1,6 @@
 import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../contexts/UserContext";
-import { getTasks } from "../utils/tasks";
+import { getTasks, addTask } from "../utils/tasks";
 
 export function Home() {
   const userContext = useContext(UserContext);
@@ -15,12 +15,16 @@ export function Home() {
     <div className="w-full h-full flex flex-col justify-center items-center">
       <h1 className="text-3xl font-bold">Your Tasks</h1>
       <section className="p-2 flex flex-col justify-between mt-16 bg-white section-shadow min-w-[20rem] md:min-w-[25rem] min-h-[25rem] md:min-h-[30rem]">
-        <form className="p-2 flex justify-between items-center">
+        <form
+          onSubmit={(e) => addTask(e, userContext, tasks, setTasks)}
+          className="p-2 flex justify-between items-center"
+        >
           <input
             className="input-shadow w-[79%] outline-0 p-1"
             type="text"
             placeholder="Your Task"
             required
+            name="title"
           />
           <button
             className="w-[19%] bg-emerald-500 hover:bg-emerald-400 transition-colors p-1 text-white"
